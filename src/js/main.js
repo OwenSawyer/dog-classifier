1180cff612617449fd608f5800c4aca39b5f4a37
Owen <owen135731@yahoo.com>
2016-06-11 17:29:22 -0400

    trying to get post to work

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,18 @@
-/*global $*/
-class Main {
-    constructor() {
-        this.initialize();
-    }
-    initialize() {
-        $.ajax({
-            url: '/api/mnist',
-            method: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify(inputs),
-            success: (data) => {
-	        $('#output').text(data);
-            },
-	    error: (err) => {
-	        alert("err");
-	    }
-        });
-    }
-}
-
 $(document).ready(function(){
   $('#click').click(function() { 
-    var main = new Main();
-    main.initialize();
-    return false;
+	alert("Here");
+	$.ajax({
+	    url: '/api/mnist',
+	    method: 'POST',
+	    contentType: 'application/json',
+	    data: JSON.stringify(inputs),
+	    success: (data) => {
+		$('#output').text(data);
+	    },
+	    error: (err) => {
+		alert("err");
+	    }
+	});
+    	return false;
   });
 });
