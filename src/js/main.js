33f57c9dd7db49ed8c1d8c4df30576b3fd414c0f
Owen <owen135731@yahoo.com>
2016-06-11 17:23:43 -0400

    whew

--- src/js/main.js
+++ src/js/main.js
@@ -11,12 +11,18 @@ class Main {
             data: JSON.stringify(inputs),
             success: (data) => {
 	        $('#output').text(data);
-            }
+            },
+	    error: (err) => {
+	        alert("err");
+	    }
         });
     }
 }
 
-$(() => {
+$(document).ready(function(){
+  $('#click').click(function() { 
     var main = new Main();
     main.initialize();
+    return false;
+  });
 });
