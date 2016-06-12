780177ab1d31df9245c4a515409c9cd3b11bbec4
owen135731 <owensawyer95@gmail.com>
2016-06-11 23:04:14 -0400

    aaa

--- main.py
+++ main.py
@@ -13,18 +13,20 @@ app = Flask(__name__)
 
 @app.route('/api/mnist', methods=['POST'])
 def mnist():
-    #input = ((255 - np.array(request.json, dtype=np.uint8)) / 255.0).reshape(1, 784)
-    #output1 = simple(input)
-    #output2 = convolutional(input)
-    #return jsonify(results=[output1, output2])
-    #with open("classify_image.py") as f:
-    #code=compile(f.read(),"classify_read.py"),'exec')
-    #return jsonify(exec(code))
-    #ret = classify_image.run_inference_on_image('cropped_panda.jpg');
-    #alert("here2");
-    #console.log("here2");
-    #return jsonify(ret)
-    return jsonify("aaa");
+    if request.headers['Content-Type'] == 'text/plain':
+        return "Text Message: " + request.data
+
+    elif request.headers['Content-Type'] == 'application/json':
+        return "JSON Message: " + json.dumps(request.json)
+
+    elif request.headers['Content-Type'] == 'application/octet-stream':
+        f = open('./binary', 'wb')
+        f.write(request.data)
+                f.close()
+        return "Binary message written!"
+
+    else:
+        return "415 Unsupported Media Type ;)"
 
 @app.route('/')
 def main():
