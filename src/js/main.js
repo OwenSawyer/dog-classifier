56c59e3d93f4fa82972bb2570711c663b14bb7d7
owen135731 <owensawyer95@gmail.com>
2016-06-11 22:19:34 -0400

    ff

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,18 @@
-$(document).ready(function(){
-  $('#click').click(function() { 
-	alert("Here");
-	$.ajax({
-	    url: '/api/mnist',
-	    method: 'GET',
-	    contentType: 'application/json',
-	    data: JSON.stringify(inputs),
-	    success: (data) => {
-		$('#output').text(data);
-	    },
-	    error: (err) => {
-		alert("err");
-	    }
-	});
-    	return false;
-  });
-});
+// $(document).ready(function(){
+//   $('#click').click(function() {
+// 	alert("Here");
+// 	$.ajax({
+// 	    url: '/api/mnist',
+// 	    method: 'GET',
+// 	    contentType: 'application/json',
+// 	    data: JSON.stringify('aaa'),
+// 	    success: (data) => {
+// 		$('#output').text(data);
+// 	    },
+// 	    error: (err) => {
+// 		alert("err");
+// 	    }
+// 	});
+//     	return false;
+//   });
+// });
