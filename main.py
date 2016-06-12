9b4d8bb4a9c5b857f0950bf794272e6cf5d3a0cb
owen135731 <owensawyer95@gmail.com>
2016-06-11 22:14:22 -0400

    post req

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/api/mnist', methods=['GET'])
+@app.route('/api/mnist', methods=['POST'])
 def mnist():
     #input = ((255 - np.array(request.json, dtype=np.uint8)) / 255.0).reshape(1, 784)
     #output1 = simple(input)
@@ -20,7 +20,9 @@ def mnist():
     #with open("classify_image.py") as f:
     #code=compile(f.read(),"classify_read.py"),'exec')
     #return jsonify(exec(code))
-    ret = classify_image.run_inference_on_image('cropped_panda.jpg')
+    ret = classify_image.run_inference_on_image('cropped_panda.jpg');
+    alert("here2");
+    console.log("here2");
     return jsonify(ret)
 
 
