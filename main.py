9be1a1048219b8ca61359abf3d00277b35cf167e
owen135731 <owensawyer95@gmail.com>
2016-06-18 20:13:21 -0400

    bar code in click

--- main.py
+++ main.py
@@ -256,9 +256,8 @@ def progress():
     def generate():
         x = 0
         while x < 100:
-            print (x)
             x = x + 10
             time.sleep(1.5)
             yield "data:" + str(x) + "\n\n"
-        return
+        return False
     return Response(generate(), mimetype= 'text/event-stream')
