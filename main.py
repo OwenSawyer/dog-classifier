5c410de61bd1c4d559ec0092b3884b1c3257aedb
owen135731 <owensawyer95@gmail.com>
2016-06-20 21:18:17 -0400

    file uploads

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from flask import Flask, jsonify, render_template, request, send_file, Response, send_from_directory
+from flask import Flask, jsonify, render_template, request, send_file, Response, send_from_directory, redirect, url_for
+from werkzeug import secure_filename
+
 import time
 import os
 import re
@@ -228,8 +230,18 @@ def fetch_picture(url):
 
     return False
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
+
+
 root = os.path.join(os.path.dirname(os.path.abspath(__file__)));
 app = Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] = 'uploads/'
+# These are the extension that we are accepting to be uploaded
+app.config['ALLOWED_EXTENSIONS'] = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
 
 @app.route('/api/classify', methods=['POST'])
 def classify():
@@ -261,3 +273,24 @@ def progress():
             yield "data:" + str(x) + "\n\n"
         return False
     return Response(generate(), mimetype= 'text/event-stream')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    # Get the name of the uploaded file
+    file = request.files['file']
+    # Check if the file is one of the allowed types/extensions
+    if file and allowed_file(file.filename):
+        # Make the filename safe, remove unsupported chars
+        filename = secure_filename(file.filename)
+        # Move the file form the temporal folder to
+        # the upload folder we setup
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        # Redirect the user to the uploaded_file route, which
+        # will basicaly show on the browser the uploaded file
+        return redirect(url_for('uploaded_file',
+                                filename=filename))
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
